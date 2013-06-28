b886dd58a71f4539d60a1b039d3c4afc1f22a724
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-06-28 22:14:14 +0200

    Prepared tests for new functionality in Spawn - tests fail

--- Expect.Test/SpawnTest.cs
+++ Expect.Test/SpawnTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Expect;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Expect.Test
 {
@@ -35,5 +36,51 @@ namespace Expect.Test
 
             Assert.IsTrue(funcCalled);
         }
+
+        [TestMethod]
+        public void SplitResultExpectTest()
+        {
+            var proc = new Mock<Process>();
+            int i = 0;
+            Task<string>[] tasks = {new Task<string>(() => "test expected "), 
+                                     new Task<string>(() => "string test")};
+            tasks[0].Start();
+            tasks[1].Start();
+
+            proc.Setup(p => p.readAsync()).Returns(() => tasks[i]).Callback(() => i++);
+            Spawn spawn = new Spawn(proc.Object);
+            bool funcCalled = false;
+
+            spawn.expect("expected string", () => funcCalled = true);
+
+            Assert.IsTrue(funcCalled);
+            Assert.AreEqual(2, i);
+        }
+
+        [TestMethod]
+        public void TimeoutThrownExpectTest()
+        {
+            var proc = new Mock<Process>();
+            Task<string> task = new Task<string>(() => "test expected string test");
+            task.Start();
+            proc.Setup(p => p.readAsync()).Callback(() => Thread.Sleep(1200)).Returns(task);
+            Spawn spawn = new Spawn(proc.Object);
+            Exception exc = null;
+            bool funcCalled = false;
+            
+            try
+            {
+                spawn.expect("expected string", () => funcCalled = true);
+            }
+            catch (Exception e)
+            {
+                exc = e;
+            }
+
+            Assert.IsNotNull(exc);
+            Assert.IsInstanceOfType(exc, typeof(TimeoutException));
+            Assert.IsFalse(funcCalled);
+        }
+
     }
 }
