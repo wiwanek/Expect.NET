f61e4e358265f3de6b9c4e0762945aa9a1179448
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-12-08 00:01:00 +0100

    Fixed EOL format

---
+++ Expect.Test/CommandBacendTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Expect.Test
+{
+    [TestClass]
+    public class CommandBacendTest
+    {
+        [TestMethod]
+        public void TestCtor()
+        {
+
+        }
+    }
+}
