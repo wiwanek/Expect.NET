f6f9212f1dcf3e4c6ac09591e03509e4885e004e
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-12-11 22:32:27 +0100

    Added new project ExampleApp

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Expect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spawn spawn = new SpawnCommand("cmd");
+        }
+    }
+}
