29e269c4431c7bfd96d2967d07dead27b7ec91ef
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2013-12-13 12:04:25 +0100

    Added ExampleApp with Expect.NET from Nuget

---
+++ ExampleAppNuget/Program.cs
@@ -0,0 +1,34 @@
+﻿using Expect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleAppNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("ExampleApp");
+                Spawn spawn = new SpawnCommand("cmd.exe");
+                spawn.expect(">", s => Console.WriteLine("got: " + s));
+                spawn.send("dir c:\\\n");
+                spawn.expect("Program", (s) => Console.WriteLine("found: " + s));
+                spawn.send("asdsdf\n");
+                spawn.expect(">", (s) => Console.WriteLine("found: " + s));
+                spawn.send("cd c:\\\n");
+                spawn.expect(@">", s => spawn.send("cd Users\n"));
+                spawn.expect(@"c:\\Users>", s => Console.WriteLine("done\n" + s));
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
