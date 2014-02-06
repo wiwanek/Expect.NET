97b7f486521a4783c86a89feef039acef506d4fc
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2014-02-06 21:08:38 +0100

    Example app in VB.NET

---
+++ ExampleAppVB/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports Expect
+
+Module Module1
+
+    Sub Main()
+        Dim spawn As Spawn = New SpawnCommand("cmd.exe")
+        Try
+            spawn.Expect(">", Sub(s) Console.WriteLine("Prompt --> " + s))
+            spawn.SetTimeout(1000)
+            spawn.Send("dir c:\" + Environment.NewLine)
+            spawn.Expect("Program Files", Sub(s) Console.WriteLine(s))
+        Catch ex As System.TimeoutException
+            Console.WriteLine("Timeout")
+        End Try
+
+        Console.WriteLine("Done")
+        Console.ReadKey()
+    End Sub
+
+End Module
