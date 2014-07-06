fe228ceba5ffd123e58b9e457d8cb156f343792c
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2014-07-06 14:25:57 +0200

    updated version in build script

--- build.ps1
+++ build.ps1
@@ -10,7 +10,7 @@ properties {
   $minor = 0
   $patch = 0
   $build = 0
-  $label = "rc"
+  $label = ""
   $version_assembly = "$major.$minor.$patch.$build"
   $version_nuget = "$major.$minor.$patch"
   if ( $label -ne "" ) {
