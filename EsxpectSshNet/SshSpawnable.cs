cd2a1ad3a1e9d837e3074cf03af7935fa50e3be6
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2014-07-07 22:03:42 +0200

    Added project ExpectSshNet

---
+++ EsxpectSshNet/SshSpawnable.cs
@@ -0,0 +1,32 @@
+﻿using ExpectNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsxpectSshNet
+{
+    public class SshSpawnable : ISpawnable
+    {
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ReadAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
