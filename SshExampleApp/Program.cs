a8200533c31bd658b03893d166bb15e412508419
Wojtek Iwanek <wojtek.iwanek@hotmail.com>
2014-07-30 00:23:11 +0200

    Exampleapp using Ssh

--- SshExampleApp/Program.cs
+++ SshExampleApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExpectNet;
+using ExpectSshNet;
+using System.Text.RegularExpressions;
 
 namespace SshExampleApp
 {
@@ -10,6 +13,16 @@ namespace SshExampleApp
     {
         static void Main(string[] args)
         {
+            Session session = Expect.Spawn(new SshSpawnable("192.168.10.150", "test", "test"));
+            session.Expect("$ ", () => session.Send("ls -al\n"));
+            session.Expect("$ ", (s) => Console.WriteLine(s));
+            session.Send("uname -a\n");
+            string text = "";
+            session.ExpectAsync("Debian", (s) => text = s);
+            bool match = new Regex(".*router.*[Dd]ebian.*").IsMatch(text);
+            Console.WriteLine("match=" + match);
+            Console.WriteLine("Done");
+            Console.ReadKey();
         }
     }
 }
